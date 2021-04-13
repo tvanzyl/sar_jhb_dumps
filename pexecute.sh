14f0adb577f9df818ee311627ed2ca06f4ef36c0
tvanzyl <tvanzyl@gmail.com>
2021-04-13 11:11:38 +0200

    added latest changes with the resample subset

---
+++ pexecute.sh
@@ -0,0 +1,25 @@
+cd /data1/data/sar_izmir/snaphu/20200918_20201117
+snaphu -f snaphu.conf Phase_ifg_VV_18Sep2020_17Nov2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20200930_20201012
+snaphu -f snaphu.conf Phase_ifg_VV_30Sep2020_12Oct2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20200930_20201024
+snaphu -f snaphu.conf Phase_ifg_VV_30Sep2020_24Oct2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20200930_20201105
+snaphu -f snaphu.conf Phase_ifg_VV_30Sep2020_05Nov2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20200930_20201117
+snaphu -f snaphu.conf Phase_ifg_VV_30Sep2020_17Nov2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20200930_20201129
+snaphu -f snaphu.conf Phase_ifg_VV_30Sep2020_29Nov2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20201012_20201024
+snaphu -f snaphu.conf Phase_ifg_VV_12Oct2020_24Oct2020.snaphu.img 8852 &
+cd -
+cd /data1/data/sar_izmir/snaphu/20201012_20201105
+snaphu -f snaphu.conf Phase_ifg_VV_12Oct2020_05Nov2020.snaphu.img 8852 &
+cd -
+wait
